028a6862dbaa64433c9be6e0b8d875b7cc5089a8
SizoDevelops <fierceincii@gmail.com>
2025-02-12 22:53:57 +0200

    Styled Emails

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,29 +1,40 @@
 
+import { Email } from '@/components/EmailComposition';
 import { NextResponse } from 'next/server';
+import path from 'path';
 const nodemailer = require("nodemailer")
+const Mailgen = require("mailgen")
+
+
+const mailGenerator = new Mailgen({
+  theme: {
+    path: path.resolve("public/assets/theme.html"),
+    plaintextPath: path.resolve("public/assets/theme.txt"),
+  },
+  product: {
+      name: "Sizo Develops",
+      link: 'https://www.sizodevelops.com',
+      logo: 'https://www.sizodevelops.com/Icons/Logo%20Black.svg'
+  }
+});
 
 export async function POST(request: Request) {
-  
+
     const body = await request.json();
-    
+
+    const email = Email(body.name);
+    var emailBody = mailGenerator.generate(email.styledEmail);
+
     const message = {
         from: `ScanA Team <${process.env.EMAIL_FROM}>`,
         to: body.email,
         subject: "Using NodeMailer With Next Api",
-        html: `
-        <p>
-          Hello ${body.name},
-        </p>
-
-        <p>
-          This is a test email sent using NodeMailer with Next Api 👍
-        </p>
-        `,
-       
+        html: emailBody,
         headers: {
           "X-Entity-Ref-ID": "newmail",
         },
       };
+
       var transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
@@ -36,8 +47,8 @@ export async function POST(request: Request) {
         },
       });
 
-      console.log(JSON.stringify(body))
-      
+    
+
       try {
   
           await transporter.sendMail(message);
